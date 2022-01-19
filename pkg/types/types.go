b0db0eceb8c2f0d397fad000dccb3f05bda01a16
Yessentemir <esentemir.utepov@gmail.com>
2022-01-20 00:17:08 +0500

    изменен модуль и типы

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 // Category представляет собой категорию, в котоой был совершен платеж(авто, аптеки, рестораны и.т.д.).
 type Category string
 
+// Status представляет собой статус платежа.
+type Status string
+
+// Предопределенные статусы платежей.
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment представляет информацию о платеже.
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
